5e7cc46fee4325fa57068f8095e580c7a96a668a
Michael Gau <61722401+mygau91@users.noreply.github.com>
2023-09-12 02:54:01 +0800

    update port

--- backend/app/start.js
+++ backend/app/start.js
@@ -1,7 +1,8 @@
-// start.js
-const app = require('./server'); // Make sure the path is correct
-const PORT = 4000;
+const app = require("./server"); // Make sure the path is correct
+
+// Use the PORT provided by the environment variable or default to 8080
+const PORT = process.env.PORT || 4000;
 
 app.listen(PORT, () => {
-  console.log(`Server is running on http://localhost:${PORT}`);
+  console.log(`Server is running on port ${PORT}`);
 });
